a60bf0a006be0d91060cd5eb02333717647fac5b
Yu Cao <tsaoyu@tsaoyu.com>
2018-04-15 20:07:40 +0100

    Upload everything

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python2 ./jemdoc -c mysite.conf *.jemdoc
+rsync *.html yc6n13@ssh.soton.ac.uk:~/public_html
+rsync  *.jpg yc6n13@ssh.soton.ac.uk:~/public_html
+rsync  *.mp3 yc6n13@ssh.soton.ac.uk:~/public_html
+rsync  *.pdf yc6n13@ssh.soton.ac.uk:~/public_html
+rsync  *.png yc6n13@ssh.soton.ac.uk:~/public_html
+rsync  *.js yc6n13@ssh.soton.ac.uk:~/public_html 
+echo "Update and upload successful!"
