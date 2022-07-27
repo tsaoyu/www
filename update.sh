fe9908284321ead9eff4bf48f6ab393c5da7d6dd
tsaoyu <tsaoyu@tsaoyu.com>
2022-07-27 22:20:46 +0800

    Update website

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 python2 ./jemdoc -c mysite.conf *.jemdoc
-rsync *.html yc6n13@ssh.soton.ac.uk:~/public_html
-rsync  *.jpg yc6n13@ssh.soton.ac.uk:~/public_html
-rsync  *.mp3 yc6n13@ssh.soton.ac.uk:~/public_html
-rsync  *.pdf yc6n13@ssh.soton.ac.uk:~/public_html
-rsync  *.png yc6n13@ssh.soton.ac.uk:~/public_html
-rsync  *.js yc6n13@ssh.soton.ac.uk:~/public_html 
-rsync  *.gif yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync *.html yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync  *.jpg yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync  *.mp3 yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync  *.pdf yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync  *.png yc6n13@ssh.soton.ac.uk:~/public_html
+# rsync  *.js yc6n13@ssh.soton.ac.uk:~/public_html 
+# rsync  *.gif yc6n13@ssh.soton.ac.uk:~/public_html
 echo "Update and upload successful!"
